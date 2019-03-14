34ed8533590446f734cd6a9465fded6d42263855
adamjanik91 <a.janik334@gmail.com>
2019-03-14 17:46:22 +0100

    base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { MaintenanceComponent } from './maintenance/maintenance.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    MaintenanceComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
